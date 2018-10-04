a222ed7d52ec4d6cc39a92483b051d498b0c8482
Laurie Egan-Carter <31734215+LaurieEgan-Carter@users.noreply.github.com>
2018-10-04 13:51:01 +0100

    Inputs changed
    Changed names for better clarity.

--- Gmail Spam Bot/g-bomber.py
+++ Gmail Spam Bot/g-bomber.py
@@ -4,8 +4,8 @@ import getpass
 import smtplib
 
 
-email = input(' Enter Your Email Address : ')
-pswd = getpass.getpass(' Enter Your Password : ')
+semail = input(' Enter sender Email Address : ')
+pswd = getpass.getpass(' Enter sender Password : ')
 vemail = input(' Enter Vicitm Email Address : ')
 message = input(' Enter Your Message Here : ')
 count = int(input(' How Many Time You Want To Send : '))
@@ -13,6 +13,8 @@ count = int(input(' How Many Time You Want To Send : '))
 print()
 print('Loading...')
 print()
+
+
 try:
     smtp_server = 'smtp.gmail.com'
     port = 587
@@ -20,12 +22,12 @@ try:
     server.ehlo()
     if smtp_server == "smtp.gmail.com":
             server.starttls()
-    server.login(email,pswd)
+    server.login(semail,pswd)
     i = 0
     print(' Sending is in Progress \n')
     while i < count:
         i+=1
-        server.sendmail(email,vemail,message)
+        server.sendmail(semail,vemail,message)
         if i == 1:
             print ('Email has been sent successfully ')
         elif i == 2:
@@ -42,8 +44,9 @@ except KeyboardInterrupt:
     print(" ")
     print (' Canceled')
     sys.exit()
+    
 except smtplib.SMTPAuthenticationError:
     print(" ")
     print(' The username or password you entered is incorrect.')
-    print (' Check if the Options of Applications are less secure is enabled ')
+    print (' Check if the option of "Allow less secure apps" is enabled on the email you want to send the message from.')
     sys.exit()
